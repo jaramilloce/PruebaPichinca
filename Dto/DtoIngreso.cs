c594018ea2f692c15b66030a019aaaf9c5ae7022
Edison Jaramillo <ewjc2003@hotmail.com>
2020-09-03 11:53:59 -0500

    Creación de Dto
    Creación de Dto

---
+++ Dto/DtoIngreso.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PruebaPichincha.Dto
+{
+    public class DtoIngreso
+    {
+        public string message { get; set; }
+        public string to { get; set; }
+        public string from { get; set; }
+        public int timeToLifeSec { get; set; }
+
+    }
+}
